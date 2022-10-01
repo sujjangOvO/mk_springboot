c8267de31543afd438046daada49e2a77519a17f
SuYeon <pnk7038@gmail.com>
2022-10-01 16:59:38 +0900

    1st commit

---
+++ src/main/java/com/example/moonkey/MoonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.moonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonkeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moonkey/MoonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
