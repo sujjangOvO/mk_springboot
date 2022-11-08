9fa122a54c18504fe279a5d3e6cf4f18334131db
Lee-wonho <gh9908@gmail.com>
2022-11-08 12:26:01 +0900

    [Modify] MenuService method editing
    register 및 list search 변경

--- src/main/java/com/example/moonkey/service/MenuService.java
+++ src/main/java/com/example/moonkey/service/MenuService.java
@@ -14,6 +14,7 @@ import com.example.moonkey.util.SecurityUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -30,16 +31,37 @@ public class MenuService {
 	}
 
 	@Transactional
-	public MenuDto register(long storeUid, MenuDto menuDto){
+	public MenuDto register(MenuDto menuDto){
+		long storeId = menuDto.getStoreId();
+		Store store = Store.builder().build();
+
+		Menu menu = Menu.builder()
+				.menuName(menuDto.getMenuName())
+				.storeId(store)
+				.description(menuDto.getDescription())
+				.options(menuDto.getOptions())
+				.price(menuDto.getPrice())
+				.options(menuDto.getOptions())
+				.build();
 
 		return new MenuDto();
 	}
 
 	public List<MenuDto> getMenu(long storeid){
-		List<Menu> menuList = menuRepository.findAllbyStoreId(storeid);
+		List<Menu> menuList = menuRepository.findAllbyStore_storeId(storeid);
+		Iterator<Menu> iter = menuList.iterator();
 
-		List<MenuDto> menuDtos;
+
+
+		List<MenuDto> menuDtos = new List<MenuDto>;
 		// TO-DO:Converting Menu list -> MenuDto list
+		while(iter.hasNext())
+		{
+			Menu menu = iter.next();
+			MenuDto menuDto;
+			menuDtos.add(menuDto);
+		}
+
 		return	menuDtos;
 	}
 
