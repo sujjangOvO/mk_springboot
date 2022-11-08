f16b7059c181370e1adaab78a9986cc86ab7d2ed
SuYeon <pnk7038@gmail.com>
2022-11-08 17:46:23 +0900

    [Modify] PartyController list method modification post -> get

--- src/main/java/com/example/moonkey/controller/PartyController.java
+++ src/main/java/com/example/moonkey/controller/PartyController.java
@@ -4,10 +4,7 @@ import com.example.moonkey.dto.PartyDto;
 import com.example.moonkey.service.PartyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,7 +18,7 @@ public class PartyController {
         this.partyService = partyService;
     }
 
-    @PostMapping("/party/list")
+    @GetMapping("/party/list")
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public ResponseEntity<List<PartyDto>> partyList(
             @Valid @RequestBody PartyDto partyDto
