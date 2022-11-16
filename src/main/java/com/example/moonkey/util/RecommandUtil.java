451ce0c60d4da809b8b8cea01b167bd447499efb
Lee-wonho <gh9908@gmail.com>
2022-11-16 14:17:03 +0900

    [Add]] Add to utils function for Collaborative Filtering
    package Draft

---
+++ src/main/java/com/example/moonkey/util/RecommandUtil.java
@@ -0,0 +1,18 @@
+package com.example.moonkey.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import com.example.moonkey.repository.AccountRepository;
+import com.example.moonkey.repository.OrderRepository;
+
+
+public class RecommandUtil {
+
+
+
+}
